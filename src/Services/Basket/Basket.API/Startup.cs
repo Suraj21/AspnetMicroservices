a676f58a3c1cd6c824d2457d588686e43101f009
Sachidanand Suraj <ssuraj@intelliflo.com>
2021-10-28 15:05:17 +0530

    basket checkout consumer implementation

--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -1,19 +1,13 @@
 using Basket.API.GrpcServices;
 using Basket.API.Repositories;
-using Discount.Grpc.Protos;
 using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using static Discount.Grpc.Protos.DiscountProtoService;
 
 namespace Basket.API
