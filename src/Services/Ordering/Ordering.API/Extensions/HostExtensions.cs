e8a55b531413ed86eaf16a77adb277cba5c43da8
Sachidanand Suraj <ssuraj@intelliflo.com>
2021-11-15 12:33:51 +0530

    Retry implementation for database migration

--- src/Services/Ordering/Ordering.API/Extensions/HostExtensions.cs
+++ src/Services/Ordering/Ordering.API/Extensions/HostExtensions.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Polly;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,11 +15,9 @@ namespace Ordering.API.Extensions
     {
         public static IHost MigrateDatabase<TContext>(this IHost host,
                 Action<TContext,
-                IServiceProvider> seeder,
-                int? retry = 0) where TContext : DbContext
+                IServiceProvider> seeder) where TContext : DbContext
         {
-            int retryForAvailability = retry.Value;
-
+           
             using var scope = host.Services.CreateScope();
             var services = scope.ServiceProvider;
             var logger = services.GetRequiredService<ILogger<TContext>>();
@@ -27,7 +26,16 @@ namespace Ordering.API.Extensions
             {
                 logger.LogInformation("Migrating database associated with context {DbContextName}", typeof(TContext).Name);
 
-                InvokeSeeder(seeder, context, services);
+                var retry = Policy.Handle<SqlException>()
+                       .WaitAndRetry(
+                           retryCount: 5,
+                           sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                           onRetry: (exception, retryCount, context) =>
+                           {
+                               logger.LogError($"Retry {retryCount} of {context.PolicyKey} at {context.OperationKey}, due to: {exception}.");
+                           });
+
+                retry.Execute(() => InvokeSeeder(seeder, context, services));
 
                 logger.LogInformation("Migrated database associated with context {DbContextName}", typeof(TContext).Name);
             }
@@ -35,12 +43,12 @@ namespace Ordering.API.Extensions
             {
                 logger.LogError(ex, "An error occurred while migrating the database used on context {DbContextName}", typeof(TContext).Name);
 
-                if (retryForAvailability < 50)
-                {
-                    retryForAvailability++;
-                    System.Threading.Thread.Sleep(2000);
-                    MigrateDatabase<TContext>(host, seeder, retryForAvailability);
-                }
+                //if (retryForAvailability < 50)
+                //{
+                //    retryForAvailability++;
+                //    System.Threading.Thread.Sleep(2000);
+                //    MigrateDatabase<TContext>(host, seeder, retryForAvailability);
+                //}
             }
             return host;
         }
