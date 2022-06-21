d070300b71252df1c0c52e057b53e7121af6067a
Sachidanand Suraj <ssuraj@intelliflo.com>
2022-06-21 13:55:38 +0530

    Api service Integration to client

--- src/Client/Movies.Client/APIServices/MovieApiService.cs
+++ src/Client/Movies.Client/APIServices/MovieApiService.cs
@@ -1,6 +1,9 @@
-﻿using Movies.Client.Models;
+﻿using IdentityModel.Client;
+using Movies.Client.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Movies.Client.APIServices
@@ -24,13 +27,55 @@ namespace Movies.Client.APIServices
 
         public async Task<IEnumerable<Movie>> GetMovies()
         {
-            var movieList = new List<Movie>();
-            movieList.Add(new Movie
+            //1 - Get Token from Identity server
+            //2. Send Request to protected API
+            //3. Deserialize object to MovieList
+
+            //1 . 
+            var apiClientCredentails = new ClientCredentialsTokenRequest
             {
-                Id = 1,
-                Name = "Movie1"
-            });
-            return await Task.FromResult(movieList);
+                Address = "https://localhost:5001/connect/token",
+
+                ClientId = "moviesClient",
+                ClientSecret = "secret",
+                Scope = "moviesAPI"
+            };
+
+            //Create a new HttpClient to talk to Identity Server
+            var client = new HttpClient();
+
+            var discovery = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+            if(discovery.IsError)
+            {
+                return null;
+            }
+
+            //2. Authenticate and get an access token from Identity Server
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(apiClientCredentails);
+            if(tokenResponse.IsError)
+            {
+                return null;
+            }
+
+            //2/ Send Request to protected API
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await apiClient.GetAsync("http://localhost:5000/api/movies");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            List<Movie> movies = JsonConvert.DeserializeObject<List<Movie>>(content);
+            return movies;
+
+            //var movieList = new List<Movie>();
+            //movieList.Add(new Movie
+            //{
+            //    Id = 1,
+            //    Name = "Movie1"
+            //});
+            //return await Task.FromResult(movieList);
         }
 
         public Task<Movie> UpdateMovie(Movie movie)
